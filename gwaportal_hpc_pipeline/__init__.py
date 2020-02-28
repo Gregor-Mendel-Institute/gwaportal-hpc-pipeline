5b6d15fc00560de87899c9f7dd54cf80f3f68416
Ümit Seren <uemit.seren@gmi.oeaw.ac.at>
2020-02-28 11:21:00 +0100

    Make GENOTYPE and OUTPUT_FOLDER mandatory

--- gwaportal_hpc_pipeline/__init__.py
+++ gwaportal_hpc_pipeline/__init__.py
@@ -6,8 +6,8 @@ HPC_USER = os.environ.get('HPC_USER',None)
 REST_HOST = os.environ['REST_HOST']
 REST_USERNAME = os.environ['REST_USERNAME']
 REST_PASSWORD = os.environ['REST_PASSWORD']
-GENOTYPE_FOLDER = os.environ.get('GENOTYPE','/net/gmi.oeaw.ac.at/gwasapp/DATA_NEW_BROWSER/PYGWAS_GENOTYPES')
-STUDY_DATA_FOLDER=os.environ.get('OUTPUT_FOLDER','/mnt/gwasapp/DATA_NEW_BROWSER/study')
+GENOTYPE_FOLDER = os.environ['GENOTYPE']
+STUDY_DATA_FOLDER=os.environ['OUTPUT_FOLDER']
 SSH_KEY_FILENAME = os.environ.get('SSH_KEY_FILENAME', None)
 
 restclient = Restclient(REST_HOST,REST_USERNAME,REST_PASSWORD)
